908bbb29b9eb5a08c195d4db1ddce2568ef7b436
Denis Rybalka notebook <denis.rubalka1@gmail.com>
2020-11-19 13:57:13 +0200

    DOM Element Selection & OOP Rendering

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,26 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.attach();
+        this.configure();
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener("submit", this.handleSubmit.bind(this));
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prjInput = new ProjectInput();
